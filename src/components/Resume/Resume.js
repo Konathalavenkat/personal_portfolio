995bb6b4b50b4b9b1282e7595d047e8032d99ab9
venkat <venkatsivaff@gmail.com>
2023-12-16 12:50:10 +0530

    updated Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Row } from 'react-bootstrap';
 const Resume = () => {
   const downloadPdf = () => {
     window.open(process.env.REACT_APP_Resume_download_link);
@@ -9,15 +10,16 @@ const Resume = () => {
       <div className='d-flex justify-content-center'>
             <button onClick={downloadPdf} className='btn btn-primary'>Download Resume</button>
       </div>
-      <div style={{ height: '1080px', overflow: 'hidden'}} className='d-flex justify-content-center'>
+      <Row style={{ height: '1080px', overflow: 'hidden'}} className='d-flex justify-content-center'>
       <iframe
         title="PDF Viewer"
         src={process.env.REACT_APP_Resume_Link}
         width={'40%'}
         height="1080px"
         style={{paddingTop:"50px",paddingBottom:"50px"}}
+        className='col-md-5 col-xs-11'
       ></iframe> 
-      </div>
+      </Row>
     </div>
   );
 };
