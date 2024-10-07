25473c47670d3bd336fd68a9ccd88ef113ccab46
venkat <venkatsivaff@gmail.com>
2024-10-07 23:24:22 +0530

    refactored certifications

--- src/components/Skills/Certifications.js
+++ src/components/Skills/Certifications.js
@@ -21,45 +21,16 @@ function Certifications(){
                 
                 </Col>
                 <Col style={{paddingTop:"20px"}}>
-
-                <div>
-                    <h3>1. MERN Fullstack internship at Ethnus Codemithra <button onClick={()=>{setc5(!c5)}} className="btn btn-outline-success">{c5?"Hide Certification":"Show Certification"}</button> <button onClick={()=>{window.open("https://drive.google.com/uc?export=download&id=1wlP0YEfxDJpQ4Wjc4OLlUFvGXSVipMtC")}} className="btn btn-outline-success">Download</button></h3>
-                    <div className="d-flex flex-row justify-content-center">
-                        {c5 && <img src="https://drive.google.com/uc?id=1gAdMQLJg3d7SmAwSBymFMriMDKqZTDf9" alt="Mern fullstack" width="80%"/>}
-                    </div>
-                </div>
-                <div>
-                    <h3>2. Sololearn Python Intermediate <button onClick={()=>{setc1(!c1)}} className="btn btn-outline-success">{c1?"Hide Certification":"Show Certification"}</button> <button onClick={()=>{window.open("https://drive.google.com/uc?export=download&id=1I_PcjwesAtK0LIYfFmgJ1VBhXTjh__OE")}} className="btn btn-outline-success">Download</button></h3>
-                    <div className="d-flex flex-row justify-content-center">
-                        {c1 && <img src="https://drive.google.com/uc?id=1cUsaKLsgYB7usfubnmsw0Saf875SNeS5" alt="python intermediate certificate" width="80%"/>}
-                    </div>
-                </div>
-                <div>
-                    <h3>3. Sololearn Java Intermediate <button onClick={()=>{setc2(!c2)}} className="btn btn-outline-success">{c2?"Hide Certification":"Show Certification"}</button> <button onClick={()=>{window.open("https://drive.google.com/uc?export=download&id=1oMFfD8HBcrUzkniqb5DmHe7yDKLTR_LZ")}} className="btn btn-outline-success">Download</button></h3>
-                    <div className="d-flex flex-row justify-content-center">
-                        {c2 && <img src="https://drive.google.com/uc?id=1h5UX-JsZ5ve-CFe3qzj9RRlt8iBbngOr" alt="java intermediate certificate" width="80%"/>}
-                    </div>
-                </div>
-                <div>
-                    <h3>4. Sololearn JavaScript Intermediate <button onClick={()=>{setc3(!c3)}} className="btn btn-outline-success">{c3?"Hide Certification":"Show Certification"}</button> <button onClick={()=>{window.open("https://drive.google.com/uc?export=download&id=1On2cNSkz0NqYfhHmhX-BC9Xp4V0037A3")}} className="btn btn-outline-success">Download</button></h3>
-                    <div className="d-flex flex-row justify-content-center">
-                        {c3 && <img src="https://drive.google.com/uc?id=1PMPubqXQTt3xMZ3Au8g2rLS_XYzeGOqs" alt="javaScript intermediate certificate" width="80%"/>}
-                    </div>
-                </div>
-                <div>
-                    <h3>5. C Programming <button onClick={()=>{setc4(!c4)}} className="btn btn-outline-success">{c4?"Hide Certification":"Show Certification"}</button> <button onClick={()=>{window.open("https://drive.google.com/uc?export=download&id=1gwn9TvQzFWRbF7G-40QDeMQ9YKjGt_yD")}} className="btn btn-outline-success">Download</button></h3>
-                    <div className="d-flex flex-row justify-content-center">
-                        {c4 && <iframe src="https://drive.google.com/file/d/1gwn9TvQzFWRbF7G-40QDeMQ9YKjGt_yD/preview" width="640" height="480" allow="autoplay"></iframe>}
-                    </div>
-                </div>
-                <div>
-                    <h3>6. Cloud Essentials <button onClick={()=>{setc6(!c6)}} className="btn btn-outline-success">{c6?"Hide Certification":"Show Certification"}</button> <button onClick={()=>{window.open("https://drive.google.com/uc?export=download&id=1M05-qwqvaV_FhQMHtrLZFA3Ie5v4uTBU")}} className="btn btn-outline-success">Download</button></h3>
-                    <div className="d-flex flex-row justify-content-center">
-                        {c6 && <iframe src="https://drive.google.com/file/d/1M05-qwqvaV_FhQMHtrLZFA3Ie5v4uTBU/preview" width="640" height="480" allow="autoplay"></iframe>}
-                    </div>
-                </div>
+                
+                <Certificate certname={"1. MERN Fullstack internship at Ethnus Codemithra"} id={"1gAdMQLJg3d7SmAwSBymFMriMDKqZTDf9"} />
+                <Certificate certname={"2. Sololearn Python Intermediate "} id={"1cUsaKLsgYB7usfubnmsw0Saf875SNeS5"} />
+                <Certificate certname={"3. Sololearn Java Intermediate "} id={"1h5UX-JsZ5ve-CFe3qzj9RRlt8iBbngOr"} />
+                <Certificate certname={"4. Sololearn JavaScript Intermediate"} id={"1PMPubqXQTt3xMZ3Au8g2rLS_XYzeGOqs"} />
+                <Certificate certname={"5. C Programming"} id={"1gwn9TvQzFWRbF7G-40QDeMQ9YKjGt_yD"} />
+                <Certificate certname={"6. Cloud Essentials "} id={"1M05-qwqvaV_FhQMHtrLZFA3Ie5v4uTBU"} />
                 <Certificate certname={"7. NodeJs Udemy Course"} id={"1WSzuBfzsnmztqEKTfuzPFq9XP7gpHMGz"} />
                 <Certificate certname={"8. Intraintech AIML Internship"} id={"1YkLvx8e2Xdw8XICxkxyTIRChPE9K9Zwm"} />
+
                 </Col>
                 <Col xs={0} md={2}>
                 
@@ -71,31 +42,10 @@ function Certifications(){
                 
                 </Col>
                 <Col style={{paddingTop:"20px"}}>
-
-                <div>
-                    <h3>1. HackOn With Amazon - Season 3 <button onClick={()=>{setp1(!p1)}} className="btn btn-outline-success">{p1?"Hide Certification":"Show Certification"}</button> <button onClick={()=>{window.open("https://drive.google.com/uc?export=download&id=1CqunmdDDuiFZffMccykitB_94WiVr-lx")}} className="btn btn-outline-success">Download</button></h3>
-                    <div className="d-flex flex-row justify-content-center">
-                        {p1 && <iframe src="https://drive.google.com/file/d/1CqunmdDDuiFZffMccykitB_94WiVr-lx/preview" width="640" height="480" allow="autoplay"></iframe>}
-                    </div>
-                </div>
-                <div>
-                    <h3>2. Flipkart GRiD 5.0 - Software Development Track <button onClick={()=>{setp2(!p2)}} className="btn btn-outline-success">{p2?"Hide Certification":"Show Certification"}</button> <button onClick={()=>{window.open("https://drive.google.com/uc?export=download&id=1Mz841JuuDSTP8wwCTP5muagbWauNr24m")}} className="btn btn-outline-success">Download</button></h3>
-                    <div className="d-flex flex-row justify-content-center">
-                        {p2 && <iframe src="https://drive.google.com/file/d/1Mz841JuuDSTP8wwCTP5muagbWauNr24m/preview" width="640" height="480" allow="autoplay"></iframe>}
-                    </div>
-                </div>
-                <div>
-                    <h3>3. Xiaomi Ode2Code 3.0 <button onClick={()=>{setp3(!p3)}} className="btn btn-outline-success">{p3?"Hide Certification":"Show Certification"}</button> <button onClick={()=>{window.open("https://drive.google.com/uc?export=download&id=1ygm4AVpkztCAhXBoqj07vmMpW7k0Exhy")}} className="btn btn-outline-success">Download</button></h3>
-                    <div className="d-flex flex-row justify-content-center">
-                        {p3 && <iframe src="https://drive.google.com/file/d/1ygm4AVpkztCAhXBoqj07vmMpW7k0Exhy/preview" width="640" height="480" allow="autoplay"></iframe>}
-                    </div>
-                </div>
-                <div>
-                    <h3>4. TVS Credit E.P.I.C 5.0 — IT Challenge <button onClick={()=>{setp4(!p4)}} className="btn btn-outline-success">{p4?"Hide Certification":"Show Certification"}</button> <button onClick={()=>{window.open("https://drive.google.com/uc?export=download&id=1awosLHQtx8ZvQaPRVKj0qqhxrGo5MkhC")}} className="btn btn-outline-success">Download</button></h3>
-                    <div className="d-flex flex-row justify-content-center">
-                        {p4 && <iframe src="https://drive.google.com/file/d/1awosLHQtx8ZvQaPRVKj0qqhxrGo5MkhC/preview" width="640" height="480" allow="autoplay"></iframe>}
-                    </div>
-                </div>
+                <Certificate certname={"1. HackOn With Amazon - Season 3 "} id={"1CqunmdDDuiFZffMccykitB_94WiVr-lx"} />
+                <Certificate certname={"2. Flipkart GRiD 5.0 - Software Development Track"} id={"1Mz841JuuDSTP8wwCTP5muagbWauNr24m"} />
+                <Certificate certname={"3. Xiaomi Ode2Code 3.0 "} id={"1ygm4AVpkztCAhXBoqj07vmMpW7k0Exhy"} />
+                <Certificate certname={"4. TVS Credit E.P.I.C 5.0 — IT Challenge "} id={"1awosLHQtx8ZvQaPRVKj0qqhxrGo5MkhC"} />
                 </Col>
                 <Col xs={0} md={2}>
                 
